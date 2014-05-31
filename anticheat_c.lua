b6f7a0fc1c6b50185200444c5643ada42adb856e
Rémi Gaffet <remi-gaffet@hotmail.fr>
2014-05-31 21:29:58 +0200

    Optimisation
    Optimisation du code. 
    
    Remerciement : Citizen pour l'aide apporté.

--- anticheat_c.lua
+++ anticheat_c.lua
@@ -1,14 +1,18 @@
+function verifyStat()
+    verifyHealth() -- Vérification de la vie
+    verifyFood() -- Vérification de la faim
+    verifyThirst() -- Vérification de la soif
+end
+setTimer(verifyStat, 5000, 0)
+
 function verifyHealth()
     triggerServerEvent("goCheckHealth", getLocalPlayer())
 end
-setTimer(verifyHealth, 1000, 0)
 
 function verifyFood()
     triggerServerEvent("goCheckFood", getLocalPlayer())
 end
-setTimer(verifyFood, 1000, 0)
 
 function verifyThirst()
     triggerServerEvent("goCheckThirst", getLocalPlayer())
 end
-setTimer(verifyThirst, 1000, 0)
