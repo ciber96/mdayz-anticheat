bce10288935ac96dcba5572405f1f09df88d6d86
PicardRemi <remi-gaffet@hotmail.fr>
2014-06-01 20:14:58 +0200

    Added new command
    - Add command : /admingod > Allows server administrators to be
    invincible.

--- anticheat_s.lua
+++ anticheat_s.lua
@@ -1,8 +1,12 @@
 function checkHealth()
     local vHealth = getElementData(source, "blood") or 0
-    if vHealth >= 12001 then
-        setElementData(source, "blood", 12000)
-        kickPlayer(source, "You use cheat")
+    local admingodmod = getElementData(source, "onAdminInGod")
+    if ( admingodmod == false ) then
+        if vHealth >= 12001 then
+            setElementData(source, "blood", 12000)
+            kickPlayer(source, "You use cheat")
+            --banPlayer(source, getRootElement(), "You are banned")
+        end
     end
 end
 addEvent("goCheckHealth", true)
@@ -10,9 +14,13 @@ addEventHandler("goCheckHealth", getRootElement(), checkHealth)
 
 function checkFood()
     local vFood = getElementData(source, "food") or 0
-    if vFood >= 101 then
-        setElementData(source, "food", 30)
-        kickPlayer(source, "You use cheat")
+    local admingodmod = getElementData(source, "onAdminInGod")
+    if ( admingodmod == false ) then
+        if vFood >= 101 then
+            setElementData(source, "food", 30)
+            kickPlayer(source, "You use cheat")
+            --banPlayer(source, getRootElement(), "You are banned")
+        end
     end
 end
 addEvent("goCheckFood", true)
@@ -20,10 +28,34 @@ addEventHandler("goCheckFood", getRootElement(), checkFood)
 
 function checkThirst()
     local vFood = getElementData(source, "thirst") or 0
-    if vFood >= 101 then
-        setElementData(source, "thirst", 30)
-        kickPlayer(source, "You use cheat")
+    local admingodmod = getElementData(source, "onAdminInGod")
+    if ( admingodmod == false ) then
+        if vFood >= 101 then
+            setElementData(source, "thirst", 30)
+            kickPlayer(source, "You use cheat")
+            --banPlayer(source, getRootElement(), "You are banned")
+        end
     end
 end
 addEvent("goCheckThirst", true)
 addEventHandler("goCheckThirst", getRootElement(), checkThirst)
+
+-- GOD MOD (Not detected by the anti-cheat)
+function adminGod(source)
+    local accName = getAccountName(getPlayerAccount(source))
+    if isObjectInACLGroup ("user."..accName, aclGetGroup ( "Admin" ) ) then
+        local pf = getElementData(source, "cmd.start")
+        if ( pf == false ) then
+            setElementData(source, "cmd.start", true)
+            setElementData(source, "onAdminInGod", true)
+            setElementData(source, "blood", 9999999999999999)
+        elseif ( pf == true ) then
+            setElementData(source, "cmd.start", false)
+            setElementData(source, "onAdminInGod", false)
+            setElementData(source, "blood", 12000)
+        end
+    else
+        outputChatBox ("You are not admin !", getRootElement(), 255, 0, 0, true )
+    end
+end
+addCommandHandler("admingod", adminGod)
