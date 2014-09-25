e386e2afe07d6b2d7d33385f82e4f0d7dc362c10
Rémi Gaffet <remi-gaffet@hotmail.fr>
2014-09-25 19:34:40 +0200

    Update anticheat_s.lua

--- anticheat_s.lua
+++ anticheat_s.lua
@@ -1,61 +1,64 @@
-function checkHealth()
-    local vHealth = getElementData(source, "blood") or 0
-    local admingodmod = getElementData(source, "onAdminInGod")
-    if ( admingodmod == false ) then
-        if vHealth >= 12001 then
-            setElementData(source, "blood", 12000)
-            kickPlayer(source, "You use cheat")
-            --banPlayer(source, getRootElement(), "You are banned")
-        end
+setTimer(function()
+    for k, player in ipairs(getElementsByType("player")) do
+        checkHealth(player)
+        checkFood(player)
+        checkThirst(player)
+        checkSlots(player)
     end
-end
-addEvent("goCheckHealth", true)
-addEventHandler("goCheckHealth", getRootElement(), checkHealth)
+end, 1000, 0)
 
-function checkFood()
-    local vFood = getElementData(source, "food") or 0
-    local admingodmod = getElementData(source, "onAdminInGod")
-    if ( admingodmod == false ) then
-        if vFood >= 101 then
-            setElementData(source, "food", 30)
-            kickPlayer(source, "You use cheat")
-            --banPlayer(source, getRootElement(), "You are banned")
-        end
+function checkHealth(thePlayer)
+    local vHealth = getElementData(thePlayer, "blood") or 0
+    if vHealth >= 12001 then
+        setElementData(thePlayer, "blood", 12000)
+        kickPlayer(thePlayer, "[AC] : Health Hack")
     end
 end
-addEvent("goCheckFood", true)
-addEventHandler("goCheckFood", getRootElement(), checkFood)
 
-function checkThirst()
-    local vFood = getElementData(source, "thirst") or 0
-    local admingodmod = getElementData(source, "onAdminInGod")
-    if ( admingodmod == false ) then
-        if vFood >= 101 then
-            setElementData(source, "thirst", 30)
-            kickPlayer(source, "You use cheat")
-            --banPlayer(source, getRootElement(), "You are banned")
-        end
+function checkFood(thePlayer)
+    local vFood = getElementData(thePlayer, "food") or 0
+    if vFood >= 101 then
+        setElementData(thePlayer, "food", 30)
+        kickPlayer(thePlayer, "[AC] : Food Hack")
     end
 end
-addEvent("goCheckThirst", true)
-addEventHandler("goCheckThirst", getRootElement(), checkThirst)
 
--- GOD MOD (Not detected by the anti-cheat)
-function adminGod(source)
-    local accName = getAccountName(getPlayerAccount(source))
-    if isObjectInACLGroup ("user."..accName, aclGetGroup ( "Admin" ) ) then
-        local pf = getElementData(source, "cmd.start")
-        if ( pf == false ) then
-            setElementData(source, "cmd.start", true)
-            setElementData(source, "onAdminInGod", true)
-            setElementData(source, "blood", 9999999999999999)
-        elseif ( pf == true ) then
-            setElementData(source, "cmd.start", false)
-            setElementData(source, "onAdminInGod", false)
-            setElementData(source, "blood", 12000)
-        end
-    else
-        outputChatBox ("You are not admin !", getRootElement(), 255, 0, 0, true )
+function checkThirst(thePlayer)
+    local vFood = getElementData(thePlayer, "thirst") or 0
+    if vFood >= 101 then
+        setElementData(thePlayer, "thirst", 30)
+        kickPlayer(thePlayer, "[AC] : Thirst Hack")
     end
 end
-addCommandHandler("admingod", adminGod)
+
+function checkSlots(thePlayer)
+    local vSlots = getElementData(thePlayer, "MAX_Slots") or 0
+    if vSlots >= 50 then
+        setElementData(thePlayer, "MAX_Slots", 0)
+        kickPlayer(thePlayer, "[AC] : Slots Hack")
+    end
+end
+
+function detectVehicleCheat(vehicle, seat, jacked)
+  if ( getElementModel(vehicle) == 432 or getElementModel(vehicle) == 425 or getElementModel(vehicle) == 501 or getElementModel(vehicle) == 564 or getElementModel(Vehicle) == 594 or getElementModel(vehicle) == 601 or getElementModel(vehicle) == 447 or getElementModel(vehicle) == 520 ) then
+    if isObjectInACLGroup ( "user." ..getAccountName(getPlayerAccount(source)), aclGetGroup ( "Everyone" ) ) then
+       kickPlayer(source, "[AC] : Vehicle Hack")
+    end
+  end
+end
+addEventHandler("onPlayerVehicleEnter", getRootElement(), detectVehicleCheat)
+
+WeaponID = {
+    [35] = true,
+    [36] = true,
+    [38] = true,
+}
+
+function detectWeaponCheat(previousWeaponID, currentWeaponID)
+  if ( WeaponID[currentWeaponID] ) then
+    if isObjectInACLGroup ( "user." ..getAccountName(getPlayerAccount(source)), aclGetGroup ( "Everyone" ) ) then
+        kickPlayer(source, "[AC] : Weapon Hack")
+    end
+  end
+end
+addEventHandler("onPlayerWeaponSwitch", getRootElement(), detectWeaponCheat)
